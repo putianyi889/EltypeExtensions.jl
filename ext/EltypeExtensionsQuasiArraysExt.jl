114e773a0f6065051dd290cd08a28c5c386eac98
Tianyi Pu <912396513@qq.com>
2024-03-14 09:03:33 +0000

    remove ext

--- ext/EltypeExtensionsQuasiArraysExt.jl
+++
@@ -1,8 +0,0 @@
-module EltypeExtensionsQuasiArraysExt
-
-import QuasiArrays: AbstractQuasiArray
-import EltypeExtensions: elconvert
-
-elconvert(::Type{T}, A::AbstractQuasiArray) where T = AbstractQuasiArray{T}(A)
-
-end # module
