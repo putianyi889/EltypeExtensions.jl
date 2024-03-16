7af6d3390d1db0b3a18b0c36196582be05b04b11
Tianyi Pu <912396513@qq.com>
2024-03-16 17:57:02 +0000

    add _to_eltype supports
    to Adjoint, Diagonal, Symmetric, SymTridiagonal, Transpose, UnitRange

--- src/EltypeExtensions.jl
+++ src/EltypeExtensions.jl
@@ -43,8 +43,11 @@ Convert type `S` to have the `eltype` of `T`.
 _to_eltype(::Type{T}, ::Type{S}) where {T,S} = eltype(S) == S ? T : MethodError(_to_eltype, T, S)
 _to_eltype(::Type{T}, ::Type{Array{S,N}}) where {T,S,N} = Array{T,N}
 _to_eltype(::Type{T}, ::Type{<:Set}) where T = Set{T}
-_to_eltype(::Type{T}, ::Type{Symmetric{S,M}}) where {T,S,M} = Symmetric{T,_to_eltype(T,M)}
-_to_eltype(::Type{T}, ::Type{<:UnitRange}) where T = UnitRange{T}
+for TYP in (Adjoint, Diagonal, Symmetric, SymTridiagonal, Transpose)
+    @eval _to_eltype(::Type{T}, ::Type{$TYP{S,M}}) where {T,S,M} = $TYP{T,_to_eltype(T,M)}
+end
+_to_eltype(::Type{T}, ::Type{<:UnitRange}) where T<:Integer = UnitRange{T}
+_to_eltype(::Type{T}, ::Type{<:UnitRange}) where T<:Real = StepRangeLen{T,Base.TwicePrecision{T},Base.TwicePrecision{T},Int}
 
 nutype(x) = nutype(typeof(x))
 nutype(T::Type) = throw(MethodError(nutype, T))
