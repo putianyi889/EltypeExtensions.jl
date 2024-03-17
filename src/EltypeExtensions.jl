09c62feef1834ae68b4de595bb30200e637ce969
Tianyi Pu <912396513@qq.com>
2024-03-17 09:15:11 +0000

    fix

--- src/EltypeExtensions.jl
+++ src/EltypeExtensions.jl
@@ -10,6 +10,9 @@ export elconvert, basetype, baseconvert, precisiontype, precisionconvert
     _to_eltype(::Type{T}, ::Type{UpperHessenberg{S,M}}) where {T,S,M} = UpperHessenberg{T,_to_eltype(T,M)}
     elconvert(::Type{T}, A::UpperHessenberg{S,M}) where {T,S,M} = UpperHessenberg{T,_to_eltype(T,M)}(A)
 end
+@static if VERSION >= v"1.9"
+    elconvert(::Type{T}, A::Bidiagonal{S,V}) where {T,S,V} = Bidiagonal{T,_to_eltype(T,V)}(A.dv, A.ev, A.uplo)
+end
 @static if VERSION >= v"1.10" # see https://github.com/JuliaLang/julia/pull/46196
     elconvert(::Type{T}, A::AbstractQ) where T = convert(AbstractQ{T}, A)
     @inline bigfloatconvert(x, prec) = BigFloat(x, precision = prec)
